a277de2ba506cbc74f86d848cf62bad1eb552822
Jordan Plows <jordan@lightapi.com>
2024-04-16 12:30:09 -0700

    dev: janitor commit

--- scraper.py
+++ scraper.py
@@ -1,8 +1,15 @@
-import requests 
+import requests
+import openai 
+
+from bs4 import BeautifulSoup
 
 response = requests.get (
     url = ""
 )
 
+soup = BeautifulSoup(response.content, 'html.parser')
+
+
+title = soup.find(id="firstHeading")
 print(response.status.code)
 
